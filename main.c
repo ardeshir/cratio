eddbbb98504be15ae9884996693cb5bb7ca078c9
Ardeshir Sepahsalar <asepahsalar@hennepintech.edu>
2015-06-04 16:55:47 +0000

    main

--- main.c
+++ main.c
@@ -9,3 +9,24 @@ typedef strut {
 ratio new(int num, int den) {
 	retrun (ratio){.num=num, .den=den, .value=num/(double)den};
 }
+
+void print_r(ratio r) {
+	printf("%i/%i = %g\n", r.num, r.den, r.value);
+}
+
+ratio add(ratio left, ratio right) {
+	return (ratio){
+		.num=left.num*right.den
+			+ right.num*left.den,
+		.den=left.den * right.den,
+                .value=left.value + right.value
+		};
+}
+
+int main(){
+	ratio twothirds = new(2,3);
+	ratio quarter= new(1,4);
+        print_r(twothirds);
+	print_r(quarter);
+	print_r(add(twothirds,quarter));
+}
